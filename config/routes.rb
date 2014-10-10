db18a772a366aec8ac8366102ed8b9d94f4e5c6d
Danny Wu <wushuo1988@hotmail.com>
2014-10-10 11:38:37 -0400

    Created items controller and index view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
+  resources :items
   devise_for :users
+
   root 'welcome#index'
   
   get 'welcome/index'
-  get 'welcome/about'
+  get 'about' => 'welcome/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
