0fc826ab0b589a88130a2e1ee3cd7770ce6d8b97
Danny Wu <wushuo1988@hotmail.com>
2014-10-11 13:32:29 -0400

    Implemented basic functionality to create a new item.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,8 +8,27 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def edit
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = 'Your new to-do was saved successfully.'
+      redirect_to @item
+    else
+      flash[:error] = 'Something went wrong. Your to-do was not saved.'
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:body)
+  end
+
 end
